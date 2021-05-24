7e226c2f80e3bf473c5c4a2e6464d73c99d2d32c
Tim Rid <6593626+timrid@users.noreply.github.com>
2021-05-24 14:42:37 +0200

    added depenency to construct==2.10.67

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     url="https://github.com/timrid/construct-typing",
     author="Tim Riddermann",
     python_requires=">=3.7",
-    install_requires=["construct==2.10.66"],
+    install_requires=["construct==2.10.67"],
     keywords=[
         "construct",
         "kaitai",
